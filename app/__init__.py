adf8580227746a276d0914c29a72f8582c81e6f3
Moody Rahman <mrahman01@stuy.edu>
2020-11-20 15:46:23 -0500

    api header

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,10 @@ from flask import *
 from urllib.request import *
 from os import environ
 app = Flask(__name__)
+header={
+	"content-type":"application/json",
+	"x-cassandra-token":environ["ASTRA_AUTHORIZATION_TOKEN"]
+}
 
 
 
