422eb530bed76a801c4056489af9d3451cc061b0
Moody Rahman <mrahman01@stuy.edu>
2020-11-20 15:47:13 -0500

    export auth token

--- env.sh
+++ env.sh
@@ -7,9 +7,9 @@ export ASTRA_DB_USERNAME=moody
 export ASTRA_DB_KEYSPACE=data
 export ASTRA_DB_PASSWORD=REPLACE_ME_WITH_PASSWORD
 
-token=$(curl --request POST \
+ASTRA_AUTHORIZATION_TOKEN=$(curl --request POST \
   --url https://$ASTRA_CLUSTER_ID-$ASTRA_CLUSTER_REGION.apps.astra.datastax.com/api/rest/v1/auth \
   --header 'Content-Type: application/json' \
-  --data '{"username":"'"$ASTRA_DB_USERNAME"'", "password":"'"$ASTRA_DB_PASSWORD"'"}')
+  --data '{"username":"'"$ASTRA_DB_USERNAME"'", "password":"'"$ASTRA_DB_PASSWORD"'"}' | jq -r '.authToken')
 
-export token
+export ASTRA_AUTHORIZATION_TOKEN
