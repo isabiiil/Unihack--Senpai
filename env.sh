955a6c8ee64418892c0748778eb6c3602f6eedb6
Moody Rahman <mrahman01@stuy.edu>
2020-11-20 15:57:54 -0500

    choose when new token

--- env.sh
+++ env.sh
@@ -5,7 +5,9 @@ export ASTRA_CLUSTER_ID=aca4e01c-1465-4012-8e84-f282757e54f9
 export ASTRA_CLUSTER_REGION=us-east1
 export ASTRA_DB_USERNAME=moody
 export ASTRA_DB_KEYSPACE=data
-export ASTRA_DB_PASSWORD=REPLACE_ME_WITH_PASSWORD
+export ASTRA_AUTHORIZATION_TOKEN
+
+if [[ $1 == "new-token" ]]; then
 
 ASTRA_AUTHORIZATION_TOKEN=$(curl --request POST \
   --url https://$ASTRA_CLUSTER_ID-$ASTRA_CLUSTER_REGION.apps.astra.datastax.com/api/rest/v1/auth \
@@ -13,3 +15,4 @@ ASTRA_AUTHORIZATION_TOKEN=$(curl --request POST \
   --data '{"username":"'"$ASTRA_DB_USERNAME"'", "password":"'"$ASTRA_DB_PASSWORD"'"}' | jq -r '.authToken')
 
 export ASTRA_AUTHORIZATION_TOKEN
+fi
