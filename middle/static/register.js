5edffc397bdbacbaf7f598e74fe25604639ae7fb
Moody Rahman <mrahman01@stuy.edu>
2020-11-21 10:09:49 -0500

    formatting

--- middle/static/register.js
+++ middle/static/register.js
@@ -3,9 +3,8 @@ function register() {
 		uname: $("#uname").val(),
 		pw1: $("#pw1").val(),
 		pw0: $("#pw0").val(),
-	}
-
-	).then(data => {
+		
+	}).then(data => {
 		$("#message").text(data["status"]);
 	}
 
