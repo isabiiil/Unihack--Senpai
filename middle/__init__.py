ba259a73028d82e6ab47520b50d24be8c46fd9ac
Moody Rahman <mrahman01@stuy.edu>
2020-11-20 19:24:28 -0500

    register logic

--- middle/__init__.py
+++ middle/__init__.py
@@ -7,5 +7,25 @@ app.secret_key = "debug"
 def index():
 	return render_template("home.html")
 
+@app.route("/register", methods=["GET", "POST"])
+def register():
+	if request.method == "POST":
+		data = request.json
+		
+		if ("uname" not in data) or ("pw0" not in data) or ("pw1" not in data):
+			return {"status":"bad json"}
+		
+		if data["pw0"] != data["pw1"]:
+			return {"status":"Password's must match"}
+
+		return {"status":"User registered"}
+	else:
+		return render_template("register.html")
+
+@app.route("/auth", methods=["POST"])
+def auth():
+
+	return request.json
+
 if __name__ == "__main__":
 	app.run()
