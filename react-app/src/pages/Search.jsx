33ce5a7c5f9421c537d6b854df6e673620a854ab
Hannah Dong <hdong@wellesley.edu>
2020-11-21 21:50:48 -0500

    the struggles of search

--- react-app/src/pages/Search.jsx
+++ react-app/src/pages/Search.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import "./Search.scss";
+import Navbar from "./../components/Navbar";
+// npm install --save-dev @iconify/react @iconify-icons/gg
+import { Icon, InlineIcon } from '@iconify/react';
+import searchIcon from '@iconify-icons/gg/search';
 
 import SearchResult from "./../components/SearchResult"
 
 export default function Search() {
   return (
     <div className="search">
+            <Navbar />
       <div className="search-bar">
-        <p>Shoujo Anime</p>
+        <p>Shoujo Anime <Icon className="icon" icon={searchIcon} /></p>
       </div>
       <SearchResult title="1" summary="yeet" />
       <SearchResult title="2" summary="yeet yeet" />
